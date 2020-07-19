267df762d63fc568a8ef5e5401789875cac44801
Oxa <oxakost@gmail.com>
2020-07-19 21:09:49 +0300

    Updates

---
+++ precache-manifest.645076af7cba188d18f54252656b0f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1b7251b21168a581785da2efd82e19",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "0975aa3fc23f1d780888",
+    "url": "/cards/static/css/main.ed2c02ef.chunk.css"
+  },
+  {
+    "revision": "7331a6ba61ee2d6c825d",
+    "url": "/cards/static/js/2.440f08c8.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/cards/static/js/2.440f08c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0975aa3fc23f1d780888",
+    "url": "/cards/static/js/main.809b4e67.chunk.js"
+  },
+  {
+    "revision": "8e9c10436e990c8ebaef",
+    "url": "/cards/static/js/runtime-main.3640371d.js"
+  }
+]);
