8e45cc237745af14933dd35a7621c7964cc14351
Oxa <oxakost@gmail.com>
2020-07-29 21:03:14 +0300

    restore-page api

--- src/n2-features/f1-auth/a3-restorePassword/RestorePassword.tsx
+++ src/n2-features/f1-auth/a3-restorePassword/RestorePassword.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {ChangeEvent, useCallback, useState} from 'react';
 import Button from "../../../n1-main/m1-ui/u3-common/c2-button/Button";
 import {AppStateType} from "../../../n1-main/m2-bll/store";
 import {connect} from "react-redux";
 import Input from "../../../n1-main/m1-ui/u3-common/c3-input/Input";
 import {login} from "../../../n1-main/m1-ui/u2-routes/routes";
 import {NavLink} from "react-router-dom";
+import {resetPasswordThunk} from "../../../n1-main/m2-bll/RestorePasswordReducer";
 
 
-type mapStateToPropsType = {
+type MapStateToPropsType = {
     buttonName: string
     buttonType: string
     loading: boolean
@@ -15,20 +16,28 @@ type mapStateToPropsType = {
     value?: string
     isAuth: boolean
 }
+type MapDispatchToPropsType = {
+    resetPasswordThunk: (email: string) => void
+}
 
 
-const RestorePassword = (props: mapStateToPropsType) => {
+const RestorePassword = (props: MapStateToPropsType & MapDispatchToPropsType) => {
+
+    const [email, setEmail] = useState('');
+
+    const setEmailCallback = useCallback((e: ChangeEvent<HTMLInputElement>): void => {
+        setEmail(e.currentTarget.value)
+    }, [setEmail])
+
+
     const onClick = () => {
-
+        props.resetPasswordThunk(email);
     }
 
-    const onChange =() => {
-
-    }
 
     return (
         <div>
-            Enter your e-mail <Input onChange={onChange} value={props.value} inputType={props.inputType[0]}/>
+            Enter your e-mail <Input onChange={setEmailCallback} value={email} inputType={props.inputType[0]}/>
 
            <NavLink to={login}> <Button onClick={onClick} loading={props.loading}
                                           buttonType={props.buttonType} buttonName={props.buttonName}/></NavLink>
@@ -36,7 +45,7 @@ const RestorePassword = (props: mapStateToPropsType) => {
         </div>
     )
 }
-const mapStateToProps = (state: AppStateType) => {
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => {
     return {
         buttonName: state.restore.buttonName,
         buttonType: state.restore.buttonType[0],
@@ -45,7 +54,7 @@ const mapStateToProps = (state: AppStateType) => {
         value: state.restore.value,
         isAuth: state.restore.isAuth
     }
-
 }
 
-export default connect(mapStateToProps, null)(RestorePassword);
+export default connect<MapStateToPropsType, MapDispatchToPropsType, {}, AppStateType>
+(mapStateToProps, {resetPasswordThunk})(RestorePassword);
